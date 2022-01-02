5fd38536c576ea84621520b6f873728b28ac82a6
baroned1707 <nguyenduykhuongtqtpy@gmail.com>
2022-01-02 15:13:01 +0700

    updatessl

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ var checkRateLimit = require("./lib/rate-limit")(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require("./lib/cors-anywhere");
 cors_proxy
 	.createServer({
-		target: "https://proxy.vuabanhmi.com",
 		ssl: {
 			key: fs.readFileSync(path.join(process.cwd(), "ssl", "private.key")),
 
